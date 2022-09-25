5adea4fedbceecf630a0bf6520da4d0c00505ddd
Ricardo Fernandes <carma.reptil.0d@icloud.com>
2022-09-24 21:37:21 -0300

    teste(AppTest): adicionando teste unitário

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -6,10 +6,10 @@ import 'react-native';
 import React from 'react';
 import App from '../src/';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-import MockedNavigator from './mock/MockedNavigator';
+import { render, screen } from '@testing-library/react-native';
 
 it('renders correctly', () => {
-  renderer.create(<MockedNavigator component={App} />);
+  render(<App />);
+
+  expect(screen.toJSON()).toMatchSnapshot();
 });
